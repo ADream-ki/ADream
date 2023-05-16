b0e00784d78ab1ada8cc1c13b6b9e00b06f20e02
Xiao <2085127827@qq.com>
2023-05-16 19:58:37 +0800

    OK

--- Controls/FrameLessHelper/frameless_helper.h
+++ Controls/FrameLessHelper/frameless_helper.h
@@ -36,6 +36,14 @@ public:
     uint borderWidth();
     uint titleHeight();
 
+    // FramelessHelper *pHelper = new FramelessHelper(this);  this指需要处理的窗体
+    // pHelper->activateOn(this);  //激活当前窗体
+    // pHelper->setTitleHeight(m_pTitleBar->height());  //设置窗体的标题栏高度
+    // pHelper->setWidgetMovable(true);  //设置窗体可移动
+    // pHelper->setWidgetResizable(true);  //设置窗体可缩放
+    // pHelper->setRubberBandOnMove(true);  //设置橡皮筋效果-可移动
+    // pHelper->setRubberBandOnResize(true);  //设置橡皮筋效果-可缩放
+
 protected:
     // 事件过滤，进行移动、缩放等
     virtual bool eventFilter(QObject *obj, QEvent *event);
@@ -44,4 +52,4 @@ private:
     FramelessHelperPrivate *d;
 };
 
-#endif //FRAMELESS_HELPER_H
+#endif // FRAMELESS_HELPER_H
